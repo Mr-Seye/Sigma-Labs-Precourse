de55ae0dc30c396dcf7579bf2d9b5b6d607cd09c
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-03 00:11:16 +0100

    Update challenge_1_task_2.py
    added comments

--- Level 1/challenge_1_task_2.py
+++ Level 1/challenge_1_task_2.py
@@ -1,15 +1,16 @@
 # python3 challenge_1_task_2.py
 # Task 2a
 
+# Ask for name input
 name = input("What is your name? ")
 
 # print(f"Hello {name}!")
 
 # Task 2b
-
+# Check to see if name is Alice or Bob, if so greet them
 if name == "Alice" or name == "Bob":
   print(f"Hello {name}!")
   
-
+# If not print unauthorised message
 else:
   print("Sorry... You're not authorised to be greeted!")
