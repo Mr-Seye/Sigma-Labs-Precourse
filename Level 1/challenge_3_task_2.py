0a04bc87b44afb0e795eb387c23a198c0b0e83cb
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-03 00:24:54 +0100

    Update challenge_3_task_2.py
    more comments

--- Level 1/challenge_3_task_2.py
+++ Level 1/challenge_3_task_2.py
@@ -1,15 +1,15 @@
 
-def ask_for_number():
+def ask_for_number(): # ask_for_number funciton with try-except input validation 
   while True:
     user_input = input("Please enter a number: ")
-    if user_input.strip() == "":
+    if user_input.strip() == "": # check for empty string
       print("Error: input cannot be empty.")
       continue
     try:
-      num = int(user_input)
+      num = int(user_input) # attempts to turn input into an int
       return num
     except ValueError:
-      print("Error: input must be an integer.")
+      print("Error: input must be an integer.") # if invalid an error is thrown
   
 
 # Main program 
@@ -26,15 +26,14 @@ for n in range(0, num + 1):
     multi_total += n
   
 # Product of 1 to n
-
 prod_total = 1
 for n in range(1, num + 1):
   prod_total *= n
 
-
+# user input for each feature
 choice = input("Choose between 'sum' for sum of 1 to n, 'multi' for sum of multiples of 3 and 5 from 1 to n or 'prod' for factorial: " )
 
-if choice.lower() == "sum":
+if choice.lower() == "sum": 
   print(f"The sum of integers from 1 to {num} is: {int(sum_to)}")
 elif choice.lower() == "multi":
   print(f"The sum of integers that are multiples of 3 or 5 from 1 to {num} are: {multi_total}")
