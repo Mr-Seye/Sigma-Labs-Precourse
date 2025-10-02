fb6994a33f59ea3217f1c2a91c1b2497b56cbd7e
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-02 02:09:09 +0100

    Create challenge_1_task_2.py
    Copying solution written on NeetoCode so that they can be uploaded via GitHub link

---
+++ Level 1/challenge_1_task_2.py
@@ -0,0 +1,15 @@
+# python3 challenge_1_task_2.py
+# Task 2a
+
+name = input("What is your name? ")
+
+# print(f"Hello {name}!")
+
+# Task 2b
+
+if name == "Alice" or name == "Bob":
+  print(f"Hello {name}!")
+  
+
+else:
+  print("Sorry... You're not authorised to be greeted!")
