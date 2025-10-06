8cf64f1eca83aa9cc4c5f882b6196a17b5d9e64a
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-07 00:58:17 +0100

    Update challenge_3_task_2.py
    fixed function functionality

--- Level 2/challenge_3_task_2.py
+++ Level 2/challenge_3_task_2.py
@@ -1,20 +1,10 @@
-
-'''
-for key, val in people[0].items():
-    print(f"Name: {people[]['first_name']}")
-
-print("Do you want to Add or Remove information?")
-
-prompt = input("Type 'Add' or 'Remove': ")
-'''
-
-
-def print_dict():
-    for index in range(len(people)):
-        for key in people[index]:
-            if (key == 'name'):
-                print(
-                    f"Name: {people[index]['name']} \nAge: {people[index]['age']} \nEmployed: {'Yes' if people[index]['employed'] else 'No'} \n")
+def print_dict(people):
+    for person in people:
+        print(
+            f"Name: {person['name']}\n"
+            f"Age: {person['age']}\n"
+            f"Employed: {'Yes' if person['employed'] else 'No'}\n"
+        )
 
 
 def ask_action():
@@ -22,30 +12,44 @@ def ask_action():
         print("What would you like to do?")
         prompt = input(
             "Please enter either 'add', 'remove' or 'quit': ").strip().lower()
+        if prompt in ("add", "remove", "quit"):
+            return prompt
+        print("Invalid input.")
 
 
-def add_person():
-    while True:
-        new_name = input(
-            "Please enter the name of the person you would like to add: ")
-        new_age = int(
-            input("Please enter the age of the person you are adding: "))
-        is_employed = input("Is this person employed? ").strip().lower()
-        if is_employed in ("y", "yes", "true", "t", "1"):
-            is_employed = True
-        elif is_employed in ("n", "no", "false", "f", "0"):
-            is_employed = False
-        else:
-            print("Invalid input.")
+def add_person(people):
+    new_name = input(
+        "Please enter the name of the person you would like to add: ")
+    new_age = int(
+        input("Please enter the age of the person you are adding: "))
+    is_employed = input("Is this person employed? ").strip().lower()
+
+    if is_employed in ("y", "yes", "true", "t", "1"):
+        is_employed = True
+
+    elif is_employed in ("n", "no", "false", "f", "0"):
+        is_employed = False
+
+    else:
+        print("Invalid input.")
+
+    person = {"name": new_name, "age": new_age, "employed": is_employed}
+    people.append(person)
+
+    print("Person added successfully!\n")
+    print_dict(people)
 
 
-def remove_person():
-    target = input("Please choose a person to remove: ")
-    for i in people:
-        if target in people[i]['name']:
-            del people[i]
-        else:
-            print("Person not found.")
+def remove_person(people):
+    target = input("Please choose a person to remove: ").strip().lower()
+    for person in people:
+        if person['name'].lower() == target:
+            people.remove(person)
+            print(f"{person['name']} has been removed\n")
+            print_dict(people)
+            break
+    else:
+        print("Person not found.\n")
 
 
 def main():
@@ -53,9 +57,11 @@ def main():
               {'name': "Tom Smith", 'age': 18, 'employed': True},
               {'name': "Mariam Coulter", 'age': 66, 'employed': False},
               {'name': "Gregory Tims", 'age': 8, 'employed': False}]
+
     print_dict(people)
+
     while True:
-        act = ask_action
+        act = ask_action()
         if act == "add":
             add_person(people)
         elif act == "remove":
@@ -63,3 +69,5 @@ def main():
         elif act == "quit":
             break
 
+
+main()
