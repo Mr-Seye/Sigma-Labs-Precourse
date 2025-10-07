0e9fe2fc561c0602b4fac5a2bca78ed390f5dfc0
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-07 18:58:22 +0100

    Update challenge_3_task_3.py
    included some comments to explain the functions

--- Level 2/challenge_3_task_3.py
+++ Level 2/challenge_3_task_3.py
@@ -1,3 +1,8 @@
+'''
+iterates through the input list converting each index to a string
+forming a dictionary from the number and its unicode counterpart 
+which is then added to the list
+'''
 def num_obj(s):
     conv_s = []
     for num in s:
