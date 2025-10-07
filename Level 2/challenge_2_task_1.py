668be13469523ebb8189bc6e18928308a01f53ed
Jordan <70783293+Mr-Seye@users.noreply.github.com>
2025-10-07 18:52:16 +0100

    Update challenge_2_task_1.py
    included some comments to explain the functions

--- Level 2/challenge_2_task_1.py
+++ Level 2/challenge_2_task_1.py
@@ -1,6 +1,6 @@
 import random
 
-
+# function to validate the user's choice input
 def get_user_opt():
     while True:
         opt = input("Type 'p' to play or 'q' to quit: ").lower()
@@ -8,18 +8,21 @@ def get_user_opt():
             return opt
         print("Input is invalid, please type 'p' or 'q'.")
 
-
+# function that generates a new random number when called
 def generate_secret_num():
     return random.randint(1, 100)
 
-
+# function that asks the user for their guess and then returns it
 def get_user_guess(attempt):
     while True:
         guess = int(
             input(f"Attempt number {attempt}: Please enter your guess: "))
         return guess
 
-
+''' 
+function that evaluates the user's guess and then compares it with the 
+secret number
+'''
 def eval_guess(user_guess, secret_num):
     if user_guess > secret_num:
         return f"Your guess of {user_guess} is too large.", False
@@ -28,10 +31,10 @@ def eval_guess(user_guess, secret_num):
     else:
         return f"You guessed correctly! The secret number was {secret_num}.", True
 
-
+# function that initialises the main game
 def play_game():
     secret_num = generate_secret_num()
-    print(f"[DEBUG] Secret number is: {secret_num}")
+    # print(f"[DEBUG] Secret number is: {secret_num}")
 
     guess_attempts = 0
     prev_guess = None
@@ -55,7 +58,7 @@ def play_game():
                 print(f"You win! You guessed it in {guess_attempts} attempts!")
             break
 
-
+# main function, is essentially the menu
 def main():
 
     print("Would you like to play a guessing game?")
